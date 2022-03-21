5698811fccf30a867570f981bad5f40b667cbb63
Rodolphe Anger <rodolphe.anger@yahoo.fr>
2022-03-21 20:01:58 +0100

    Implémentation  signup

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const saucesRoutes = require("./routes/sauces");
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongoDb réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", saucesRoutes);
+
+module.exports = app;
