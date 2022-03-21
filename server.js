46f5d9e442dec3eedfe14c1958e1bb08ffc98bd0
Rodolphe Anger <rodolphe.anger@yahoo.fr>
2022-03-21 20:01:58 +0100

    Avancé jusqu'à sign up

---
+++ server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const http = require("http");
+const app = require("./app");
+
+app.set("port", process.env.APP_PORT);
+const server = http.createServer(app);
+
+server.listen(process.env.APP_PORT);
