b0166d780c54d86432419b5f077b21d6c76fcd5d
Rodolphe Anger <rodolphe.anger@yahoo.fr>
2022-03-22 18:31:42 +0100

    Implémentation routes sauces GET, PUT et DELETE

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const saucesRoutes = require("./routes/sauces");
+const path = require("path");
 
 mongoose
   .connect(
@@ -30,6 +31,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/auth", userRoutes);
 app.use("/api/sauces", saucesRoutes);
 
--- models/Sauce.js
+++ models/Sauce.js
@@ -7,10 +7,11 @@ const sauceSchema = mongoose.Schema({
   mainPepper: { type: String, required: true },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: { type: String, required: true },
-  usersDisliked: { type: String, required: true },
+  userId: { type: String, required: true },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: [{ type: String, default: "" }],
+  usersDisliked: [{ type: String, default: "" }],
 });
 
 module.exports = mongoose.model("Sauce", sauceSchema);
