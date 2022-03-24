ae9064a4e58b89cb51f3104dc63ce338be752194
Rodolphe Anger <rodolphe.anger@yahoo.fr>
2022-03-24 17:37:07 +0100

    Modif config/database.js avec async/await + try/catch

--- config/database.js
+++ config/database.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("Connexion à MongoDb réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
+(async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("Connexion à MongoDb réussie !");
+  } catch {
+    console.log("Connexion à MongoDB échouée !");
+  }
+})();
